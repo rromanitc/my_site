a25dffe3f60bd89935e5aed1dadea1b9c7287ad1
Roman <romaniv01@gmail.com>
2018-02-05 00:10:18 +0000

    update

---
+++ js/index-form.js
@@ -0,0 +1,23 @@
+$('#submit-form').click(function(ev){
+    if ($('form')[0].checkValidity()) {
+        ev.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/romaniv01@gmail.com", 
+            method: "POST",
+            data: {
+                clientName: $('#client-name').val(),
+				clientEmail: $('#client-email').val(),
+				clientPhone: $('#client-phone').val(),
+				clientMessage: $('#client-message').val(),
+            },
+            dataType: "json"
+        }).done(function() {
+			alert("Thank you for contacting us. We will call you back soon!");
+			$('#my-form')[0].reset();
+        }).fail(function(){
+            alert("Error");
+        });
+    }
+});
+
+
